cb3c0e31d6b1c60ade09e7888443a7f6e845bf0a
Josh Koenig <josh@getpantheon.com>
2015-12-14 13:02:48 -0800

    Getting clever with binding and git data

--- new_relic_deploy/nr_deploy_log.php
+++ new_relic_deploy/nr_deploy_log.php
@@ -1,28 +1,37 @@
 <?php
+// TODO: this header should not be required...
 header('Content-Type: text/plain; charset=UTF-8');
-// Fetch all the metadata about this environment from Pantheon
-$req = pantheon_curl('https://api.live.getpantheon.com/sites/self/bindings', NULL, 8443);
+// Fetch metadata
+$req = pantheon_curl('https://api.live.getpantheon.com/sites/self/bindings?type=newrelic', NULL, 8443);
 $meta = json_decode($req['body'], true);
+
+// Get the right binding for the current ENV.
+// TODO: scope down the pantheon_curl() more.
+// It should be possible to just fetch the one for the current env. 
 $nr = FALSE;
-// Filter to just the New Relic data
 foreach($meta as $data) {
-  if ($data['type'] == 'newrelic') {
+  if ($data['environment'] === PANTHEON_ENVIRONMENT) {
     $nr = $data;
     break;
   }
 }
 
+// Find out what tag we are on
+$deploy_tag = `git describe --tags`;
+// Get the annotation
+$annotation = `git tag -l -n99 $deploy_tag`;
+
+// Use New Relic's own example curl for ease of use.
 if ($nr) {
-  // This example uses New Relic's own example code.
-  // A more sophisticated example could use curl() directly.
-  // @TODO: populate the revision and changelog data.
   $curl = 'curl -H "x-api-key:'. $data['api_key'] .'"';
   $curl .= ' -d "deployment[application_id]=' . $data['app_name'] .'"';
-  $curl .= ' -d "deployment[description]=Deployment automatically logged via Quicksilver"';
-  // $curl .= ' -d "deployment[revision]=2"'; 
-  // $curl .= ' -d "deployment[changelog]=many hands make light work"';
+  $curl .= ' -d "deployment[description]=This deploy log was sent using Quicksilver"';
+  $curl .= ' -d "deployment[revision]='. $deploy_tag .'"';
+  $curl .= ' -d "deployment[changelog]='. $annotation .'"';
   $curl .= ' -d "deployment[user]='. $_POST['user_email'] .'"';
   $curl .= ' https://api.newrelic.com/deployments.xml';
+  // The below can be helpful debugging.
+  // echo "\n\nCURLing... \n\n$curl\n\n";
   passthru($curl);
 }
 else {
