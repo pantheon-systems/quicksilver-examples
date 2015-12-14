f9b6fac0c776f26182b22b660cd27ef71a9ce31e
Steve Persch <stevector@users.noreply.github.com>
2015-12-13 18:23:15 -0600

    Changing bootstrap level

--- scripts/db_sanitization.php
+++ scripts/db_sanitization.php
@@ -12,7 +12,7 @@ if (defined('PANTHEON_ENVIRONMENT') && (PANTHEON_ENVIRONMENT !== 'live')) {
   // Bootstrap Drupal using the same code as is in index.php.
   define('DRUPAL_ROOT', getcwd());
   require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
-  drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+  drupal_bootstrap(DRUPAL_BOOTSTRAP_DATABASE);
   // From http://crackingdrupal.com/blog/greggles/creating-sanitized-drupal-database-dump#comment-164
   db_query("UPDATE users SET mail = CONCAT(name, '@localhost'), init = CONCAT(name, '@localhost'), pass = MD5(CONCAT('MILDSECRET', name));");
 }
