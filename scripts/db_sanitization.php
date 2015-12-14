7019635ef84ca8ee46584e2f7df3a8b23c23b584
Steve Persch <stevector@users.noreply.github.com>
2015-12-13 18:45:03 -0600

    Removing unneeded todo

--- scripts/db_sanitization.php
+++ scripts/db_sanitization.php
@@ -5,8 +5,6 @@ header('Content-Type: text/plain; charset=UTF-8');
 // Don't ever santize the database on the live environment. Doing so would
 // destroy the canonical version of the data.
 if (defined('PANTHEON_ENVIRONMENT') && (PANTHEON_ENVIRONMENT !== 'live')) {
-  // @todo add a check to ensure that this script is actually initiated through
-  // quicksilver and not over the public web.
   // Bootstrap Drupal using the same code as is in index.php.
   define('DRUPAL_ROOT', getcwd());
   require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
