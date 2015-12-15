e36644fc61dbcc1c41ac9ae42f81130af18514f1
Steve Persch <stevector@users.noreply.github.com>
2015-12-14 19:10:23 -0600

    Change getcwd() to $_SERVER['DOCUMENT_ROOT']

--- db_sanitization/db_sanitization_drupal.php
+++ db_sanitization/db_sanitization_drupal.php
@@ -5,8 +5,8 @@ header('Content-Type: text/plain; charset=UTF-8');
 // Don't ever santize the database on the live environment. Doing so would
 // destroy the canonical version of the data.
 if (defined('PANTHEON_ENVIRONMENT') && (PANTHEON_ENVIRONMENT !== 'live')) {
-  // Bootstrap Drupal using the same code as is in index.php.
-  define('DRUPAL_ROOT', getcwd());
+  // Bootstrap Drupal using the same technique as is in index.php.
+  define('DRUPAL_ROOT', $_SERVER['DOCUMENT_ROOT']);
   require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
   drupal_bootstrap(DRUPAL_BOOTSTRAP_DATABASE);
   // From http://crackingdrupal.com/blog/greggles/creating-sanitized-drupal-database-dump#comment-164
