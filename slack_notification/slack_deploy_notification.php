35137a06227c1067aa3d97add71c16209b681124
Jordan Ryan <jordan@facetinteractive.com>
2016-01-23 00:14:32 -0800

    Missing . when constructing pantheon site url

--- slack_notification/slack_deploy_notification.php
+++ slack_notification/slack_deploy_notification.php
@@ -28,7 +28,7 @@ $fields = array(
   ),
   array( // Render Environment name with link to site, <http://{ENV}-{SITENAME}.pantheon.io|{ENV}>
     'title' => 'Environment',
-    'value' => '<http://' . $_ENV['PANTHEON_ENVIRONMENT'] . '-' . $_ENV['PANTHEON_SITE_NAME'] . 'pantheon.io|' . $_ENV['PANTHEON_ENVIRONMENT'] . '>',
+    'value' => '<http://' . $_ENV['PANTHEON_ENVIRONMENT'] . '-' . $_ENV['PANTHEON_SITE_NAME'] . '.pantheon.io|' . $_ENV['PANTHEON_ENVIRONMENT'] . '>',
     'short' => 'true'
   ),
   array( // Render Name with link to Email from Commit message
