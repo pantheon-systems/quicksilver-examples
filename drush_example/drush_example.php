81e0e8b31541101cd9f3d78063a4a12663ac0ece
Mike Milano <coder1@gmail.com>
2016-01-28 11:41:30 -0800

    fixed use of  as an object vs array

--- drush_example/drush_example.php
+++ drush_example/drush_example.php
@@ -7,15 +7,15 @@
 
 // The clone_database may be triggered on any environment, but we only want
 // to automatically enable the devel module when this event happens on dev.
-if (defined('PANTHEON_ENVIRONMENT') && (PANTHEON_ENVIRONMENT == 'dev')) {
+if (defined('PANTHEON_ENVIRONMENT') && PANTHEON_ENVIRONMENT == 'dev') {
   // First, let's retrieve a list of disabled modules with drush pm-list.
   // shell_exec() will return the output of an executable as a string.
   // Pass the --format=json flag into the drush command so the output can be converted into an array with json_decode().
-  $disabled_modules = json_decode(shell_exec('drush drush pm-list --status=disabled --format=json'));
+  $modules = json_decode(shell_exec('drush pm-list --format=json'));
 
-  // Now let's enable devel if it is disabled.
-  if ($disabled_modules['devel']) {
-    // This time let's just passthru() on the drush command so the output prints to the workflow output
-    passthru('drush pm-disable -y devel');
+  // Now let's enable devel if it is installed and not already enabled.
+  if (isset($modules->devel) && $modules->devel->status !== 'Enabled') {
+    // This time let's just passthru() to run the drush command so the command output prints to the workflow log.
+    passthru('drush pm-enable -y devel');
   }
 }
