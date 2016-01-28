5c8526a1abf0da13706faf159eb476db457df8a1
Mike Milano <coder1@gmail.com>
2016-01-28 10:05:55 -0800

    Adding drush example

---
+++ drush_example/README.md
@@ -0,0 +1,27 @@
+# Drush Example #
+
+This script demonstrates how to use drush from within a Quicksilver script.
+
+The demonstration enables the [devel](https://www.drupal.org/project/devel) module after a database has been cloned to the `dev` environment.
+
+## Instructions ##
+
+- Be sure the [devel](https://www.drupal.org/project/devel) module is installed into your Drupal codebase.
+- Copy `drush_example` directory to the private/scripts directory of your code repository.
+- Add a Quicksilver operation to your pantheon.yml to fire the script after a clone_database.
+- Test a database clone to dev from an environment where the devel module is not enabled.
+
+### Example `pantheon.yml` ###
+
+Here's an example of what your `pantheon.yml` would look like if this were the only Quicksilver operation you wanted to use:
+
+```yaml
+api_version: 1
+
+workflows:
+  clone_database:
+    after:
+      - type: webphp
+        description: Drush Example
+        script: private/scripts/drush_example/drush_example.php
+```
---
+++ drush_example/drush_example.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This example enables the devel module when a database is cloned to the dev environment.
+ *
+ * This script should be configured into the clone_database operation in pantheon.yml
+ */
+
+// The clone_database may be triggered on any environment, but we only want
+// to automatically enable the devel module when this event happens on dev.
+if (defined('PANTHEON_ENVIRONMENT') && (PANTHEON_ENVIRONMENT == 'dev')) {
+  // First, let's retrieve a list of disabled modules with drush pm-list.
+  // shell_exec() will return the output of an executable as a string.
+  // Pass the --format=json flag into the drush command so the output can be converted into an array with json_decode().
+  $disabled_modules = json_decode(shell_exec('drush drush pm-list --status=disabled --format=json'));
+
+  // Now let's enable devel if it is disabled.
+  if ($disabled_modules['devel']) {
+    // This time let's just passthru() on the drush command so the output prints to the workflow output
+    passthru('drush pm-disable -y devel');
+  }
+}
