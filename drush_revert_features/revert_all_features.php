6f7c5241f2170428fcf277bf7f536c3d58aa5d41
Greg Anderson <greg.1.anderson@greenknowe.org>
2016-02-05 16:12:44 -0800

    Adjust status messages in 'drush revert features' example.

--- drush_revert_features/revert_all_features.php
+++ drush_revert_features/revert_all_features.php
@@ -1,11 +1,11 @@
 <?php
 
 //Revert all features
-echo "Reverting all features...";
+echo "Reverting all features...\n";
 passthru('drush fra -y');
-echo "Reverting complete...";
+echo "Reverting complete.\n";
 
 //Clear all cache
-echo "Clearing cache...";
+echo "Clearing cache.\n";
 passthru('drush cc all');
-echo "Clearing cache complete...";
+echo "Clearing cache complete.\n";
