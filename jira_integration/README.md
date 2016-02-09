97ef4f9a719f35f67ee22b962de72053c37b5af0
Greg Anderson <greg.1.anderson@greenknowe.org>
2016-02-08 16:31:47 -0800

    Refactor Jira integration example; post comments for every dev or multidev environment that receives a commit.

--- jira_integration/README.md
+++ jira_integration/README.md
@@ -2,9 +2,28 @@
 
 This example parses commit messages for Jira issue IDs and adds the commit message as a comment in the related Jira issue.
 
+Example comments:
+
+  MYPROJECT-9: Adjust layout spacing.
+  Fixes issues MYPROJECT-4 and MYPROJECT-7.
+
+Commits that contain multiple Jira issues will post comments to each issue mentioned. A comment will be added each time a commit is pushed to any dev or multidev branch; each Jira comment is labeled with the appropriate commit hash and Pantheon environment that triggered the post.
+
 ## Instructions ##
 
-- Copy `jira_integration.json` to `files/private` after updating it with your credentials and Jira url.
+- Copy your Jira credentials and the URL to your Jira instance into a file called `secrets.json` and store it in the private files area of your site
+
+```shell
+  $> php -r "print json_encode(['jira_url'=>'https://myjira.atlassian.net','jira_user'=>'serviceaccount','jira_pass'=>'secret']);" > secrets.json
+  # Note, you'll need to copy the secrets into each environment where you want to save commit messages to Jira
+  $> `terminus site connection-info --env=dev --site=your-site --field=sftp_command`
+      Connected to appserver.dev.d1ef01f8-364c-4b91-a8e4-f2a46f14237e.drush.in.
+  sftp> cd files
+  sftp> mkdir private
+  sftp> cd private
+  sftp> put secrets.json
+
+```
 - Add the example `jira_integration.php` script to the `private` directory of your code repository.
 - Add a Quicksilver operation to your `pantheon.yml` to fire the script after a deploy.
 - Push code with a commit message containing a Jira issue ID!
--- jira_integration/jira_integration.php
+++ jira_integration/jira_integration.php
@@ -1,103 +1,112 @@
 <?php
-// TODO: this header should not be required...
-header('Content-Type: text/plain; charset=UTF-8');
 
-// Only watch the dev environment
-if ($_ENV['PANTHEON_ENVIRONMENT'] != 'dev') {
-  die();
+// Get the environment; we will post a new comment to Jira each time
+// a commit appears on a new branch on Pantheon.
+$env = $_ENV['PANTHEON_ENVIRONMENT'];
+
+// Do not watch test or live, though.
+if (($env == 'live') || ($env == 'test')) {
+  exit(0);
 }
 
-// Retrieve jira destination and credentials
-$jira = json_decode(file_get_contents($_SERVER['HOME'] . '/files/private/jira_integration.json'), 1);
-if ($jira == FALSE) {
-  die('No jira file found. Aborting!');
-}
-
-$commits = array(
-  // The latest git hash
-  'current' => NULL,
-  // The hash at the last time we processed this script
-  'last_processed' => NULL,
-  // Raw output of git log since the last processed
-  'history_raw' => NULL,
-  // Formatted array of commits being sent to jira
-  'history' => array(),
-  // An array keyed by jira ticket id, each holding an
-  // array of commit ids.
-  'jira' => array()
-);
+// Look up the secrets from the secrets file.
+$secrets = _get_secrets(array('jira_url', 'jira_user', 'jira_pass'), array());
 
 // Get latest commit
-$commits['current'] = shell_exec('git rev-parse HEAD');
-
+$current_commithash = shell_exec('git rev-parse HEAD');
+$last_commithash = FALSE;
 // Retrieve the last commit processed by this script
-$commit_file = $_SERVER['HOME'] . '/files/private/jira_integration.commit';
+$commit_file = $_SERVER['HOME'] . "/files/private/{$env}_jira_integration_commit.txt";
 if (file_exists($commit_file)) {
-  $commits['last_processed'] = trim(file_get_contents($commit_file));
-}
-
-// Update the last commit file with the latest commit
-file_put_contents($commit_file, $commits['current'], LOCK_EX);
-
-// Retrieve git log for commits after last processed, to current
-$cmd = 'git log'; // add -p to include diff
-if (!$commits['last_processed']) {
-  $cmd .= ' -n 1';
-}
-else {
-  $cmd .= ' ' . $commits['last_processed'] . '...' . $commits['current'];
-}
-
-$commits['history_raw'] = shell_exec($cmd);
-
-// Parse raw history into an array of commits
-$history = preg_split('/^commit /m', $commits['history_raw'], -1, PREG_SPLIT_NO_EMPTY);
-foreach ($history as $str) {
-  $commit = array(
-    'full' => 'Commit: ' . $str
-  );
-
-  // Only interested in the lines before the diff now
-  $lines = explode("\n", $str);
-  $commit['id'] = $lines[0];
-  $commit['message'] = trim(implode("\n", array_slice($lines, 4)));
-  $commit['formatted'] = '{panel:title=Commit: ' . substr($commit['id'], 0, 10) . '|borderStyle=dashed|borderColor=#ccc|titleBGColor=#e5f2ff|bgColor=#f2f2f2}
-  ' . $commit['message'] . '
-  ~' . $lines[1] . ' - ' . $lines[2] . '~
-  {panel}';
-
-  // Look for matches on a Jira issue ID format
-  // TODO: Fix - commas after issue id fail
-  preg_match('/(?:\s|^)([A-Z]+-[0-9]+)(?=\s|$)/i', $commit['message'], $matches);
-  if (count($matches) > 0) {
-    // Build the $commits['jira'] array so there is
-    // only 1 item per ticket id
-    foreach ($matches as $ticket_id) {
-      $ticket_id = strtoupper($ticket_id);
-      if (!isset($commits['jira'][$ticket_id])) {
-        $commits['jira'][$ticket_id] = array();
-      }
-      // ... and only 1 item per commit id
-      $commits['jira'][$ticket_id][$commit['id']] = $commit['id'];
-    }
-
-    // Add the commit to the history array since there was a match.
-    $commits['history'][$commit['id']] = $commit;
+  $last_processed_commithash = trim(file_get_contents($commit_file));
+  // We should (almost) always find our last commit still in the repository;
+  // if the user has force-pushed a branch, though, then our last commit
+  // may be overwritten.  If this happens, only process the most recent commit.
+  exec("git rev-parse $last_processed_commithash 2> /dev/null", $output, $status);
+  if (!$status) {
+    $last_commithash = $last_processed_commithash;
   }
 }
+// Update the last commit file with the latest commit
+file_put_contents($commit_file, $current_commithash, LOCK_EX);
+
+// Retrieve git log for commits after last processed, to current
+$commits = _get_commits($current_commithash, $last_commithash, $env);
 
 // Check each commit message for Jira ticket numbers
 foreach ($commits['jira'] as $ticket_id => $commit_ids) {
   foreach ($commit_ids as $commit_id) {
-    send_commit($jira, $ticket_id, $commits['history'][$commit_id]);
+    send_commit($secrets, $ticket_id, $commits['history'][$commit_id]);
   }
 }
 
-function send_commit($jira, $ticket_id, $commit) {
+/**
+ * Do git operations to find all commits between the specified commit hashes,
+ * and return an associative array containing all applicable commits that
+ * contain references to Jira issues.
+ */
+function _get_commits($current_commithash, $last_commithash, $env) {
+  $commits = array(
+    // Raw output of git log since the last processed
+    'history_raw' => NULL,
+    // Formatted array of commits being sent to jira
+    'history' => array(),
+    // An array keyed by jira ticket id, each holding an
+    // array of commit ids.
+    'jira' => array()
+  );
+
+  $cmd = 'git log'; // add -p to include diff
+  if (!$last_commithash) {
+    $cmd .= ' -n 1';
+  }
+  else {
+    $cmd .= ' ' . $last_commithash . '...' . $current_commithash;
+  }
+  $commits['history_raw'] = shell_exec($cmd);
+  // Parse raw history into an array of commits
+  $history = preg_split('/^commit /m', $commits['history_raw'], -1, PREG_SPLIT_NO_EMPTY);
+  foreach ($history as $str) {
+    $commit = array(
+      'full' => 'Commit: ' . $str
+    );
+    // Only interested in the lines before the diff now
+    $lines = explode("\n", $str);
+    $commit['id'] = $lines[0];
+    $commit['message'] = trim(implode("\n", array_slice($lines, 4)));
+    $commit['formatted'] = '{panel:title=Commit: ' . substr($commit['id'], 0, 10) . ' [' . $env . ']|borderStyle=dashed|borderColor=#ccc|titleBGColor=#e5f2ff|bgColor=#f2f2f2}
+    ' . $commit['message'] . '
+    ~' . $lines[1] . ' - ' . $lines[2] . '~
+    {panel}';
+    // Look for matches on a Jira issue ID format
+    // Expected pattern: "PROJECT-ID: comment".
+    preg_match('/([A-Z]+-[0-9]+)/i', $commit['message'], $matches);
+    if (count($matches) > 0) {
+      // Build the $commits['jira'] array so there is
+      // only 1 item per ticket id
+      foreach ($matches as $ticket_id) {
+        $ticket_id = strtoupper($ticket_id);
+        if (!isset($commits['jira'][$ticket_id])) {
+          $commits['jira'][$ticket_id] = array();
+        }
+        // ... and only 1 item per commit id
+        $commits['jira'][$ticket_id][$commit['id']] = $commit['id'];
+      }
+      // Add the commit to the history array since there was a match.
+      $commits['history'][$commit['id']] = $commit;
+    }
+  }
+  return $commits;
+}
+
+/**
+ * Send commits to Jira
+ */
+function send_commit($secrets, $ticket_id, $commit) {
   $payload = json_encode(array('body' => $commit['formatted']));
   $ch = curl_init();
-  curl_setopt($ch, CURLOPT_URL, $jira['url'] . '/rest/api/2/issue/' . $ticket_id . '/comment');
-  curl_setopt($ch, CURLOPT_USERPWD, $jira['user'] . ':' . $jira['pass']);
+  curl_setopt($ch, CURLOPT_URL, $secrets['jira_url'] . '/rest/api/2/issue/' . $ticket_id . '/comment');
+  curl_setopt($ch, CURLOPT_USERPWD, $secrets['jira_user'] . ':' . $secrets['jira_pass']);
   curl_setopt($ch, CURLOPT_POST, 1);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
   curl_setopt($ch, CURLOPT_TIMEOUT, 5);
@@ -108,4 +117,28 @@ function send_commit($jira, $ticket_id, $commit) {
   print("RESULT: $result");
   print("\n===== Post Complete! =====\n");
   curl_close($ch);
+}
+
+/**
+ * Get secrets from secrets file.
+ *
+ * @param array $requiredKeys  List of keys in secrets file that must exist.
+ */
+function _get_secrets($requiredKeys, $defaults)
+{
+  $secretsFile = $_SERVER['HOME'] . '/files/private/secrets.json';
+  if (!file_exists($secretsFile)) {
+    die('No secrets file found. Aborting!');
+  }
+  $secretsContents = file_get_contents($secretsFile);
+  $secrets = json_decode($secretsContents, 1);
+  if ($secrets == FALSE) {
+    die('Could not parse json in secrets file. Aborting!');
+  }
+  $secrets += $defaults;
+  $missing = array_diff($requiredKeys, array_keys($secrets));
+  if (!empty($missing)) {
+    die('Missing required keys in json secrets file: ' . implode(',', $missing) . '. Aborting!');
+  }
+  return $secrets;
 }
