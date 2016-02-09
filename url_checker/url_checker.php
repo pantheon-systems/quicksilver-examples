f56669b3ac2d936ec09f8b82429c218863312c3a
Greg Anderson <greg.1.anderson@greenknowe.org>
2016-02-09 10:29:15 -0800

    More cleanup.

--- url_checker/url_checker.php
+++ url_checker/url_checker.php
@@ -1,22 +1,26 @@
 <?php
 
-// Read our configuration file (or die)
-$config = url_checker_get_config();
-
 // Make note of the site and environment we are running on.
 $env = $_ENV['PANTHEON_ENVIRONMENT'];
 $site = $_ENV['PANTHEON_SITE_NAME'];
 
+// Read our configuration file (or die)
+$config = url_checker_get_config();
+
 // If we are not running in the live environment, or if there
 // is no 'base_url' defined in config.json, then generate an
 // appropriate URL for this current environment.
-if ($_ENV['PANTHEON_ENVIRONMENT'] != 'live') {
+if (($_ENV['PANTHEON_ENVIRONMENT'] != 'live') || !is_set($config['base_url'])) {
   $config['base_url'] = 'http://' . $env . '-' . $site . '.pantheon.io';
 }
 
 // If the base url does not end in a '/', then add one to the end.
 if ($config['base_url'][strlen($config['base_url'])] != '/') {
   $config['base_url'] .= '/';
+}
+
+if (!isset($config['check_paths']) || empty($config['check_paths'])) {
+  die('Must define configuration paths to test in "check_paths" element of config.json.');
 }
 
 $failed = 0;
@@ -36,7 +40,9 @@ foreach ($config['check_paths'] as $path) {
 $output = url_checker_build_output($results, $failed);
 print $output;
 
-if ($failed > 0) {
+// If there were any URLs that could not be accessed, and if an email
+// address is configured in config.json, then send a notification email.
+if (($failed > 0) && (isset($config['email']))) {
   $subject = 'Failed status check (' . $failed . ')';
   $message = "Below is a list of each tested url and its status:\n\n";
   $message .= $output;
