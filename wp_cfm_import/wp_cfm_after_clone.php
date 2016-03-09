03f220b91208f1d8ecbe10251fb6403cf01d4fe4
Grant McInnes <grant.mcinnes@eyesopen.ca>
2016-03-09 09:11:34 -0500

    Fix whitespace

--- wp_cfm_import/wp_cfm_after_clone.php
+++ wp_cfm_import/wp_cfm_after_clone.php
@@ -25,8 +25,8 @@ if (defined('PANTHEON_ENVIRONMENT') && (PANTHEON_ENVIRONMENT !== 'live')) {
   # way to do that is to add a mu-plugin that uses the `wpcfm_content_dir`
   # directory filter to alter the path.  See the README.md for more details. 
   $config_map = [ 'test' => 'pantheon_test',
-									'dev'  => 'pantheon_dev',
-									'default' => 'pantheon_dev' ];
+		  'dev'  => 'pantheon_dev',
+		  'default' => 'pantheon_dev' ];
   $config_name = array_key_exists(PANTHEON_ENVIRONMENT, $config_map) ? $config_map[PANTHEON_ENVIRONMENT] : $config_map['default'];
  
   my_wp_cfm_runcmd("wp plugin activate wp-cfm 2>&1",
