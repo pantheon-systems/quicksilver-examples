a68b6080196b6d8be253d8c4a1fa6c968741e609
ataylorme <andrew@ataylor.me>
2016-08-22 15:04:20 -0500

    Fetch app name from Pantheon; Pull apdex_t from existing New Relic settings

--- new_relic_apdex_t/new_relic_apdex_t.php
+++ new_relic_apdex_t/new_relic_apdex_t.php
@@ -5,115 +5,165 @@
  * Sets New Relic Apdex T values for newly created multidev environments.
  */
 
-/**
- * CHANGE THESE VARIABLES FOR YOUR OWN SITE.
- */
+// get New Relic info from the dev environment
+// Change to test or live as you wish
+$app_info = get_app_info( 'dev' );
+
+$settings = $app_info['settings'];
+
 // The "t" value (number of seconds) for your server-side apdex.
 // https://docs.newrelic.com/docs/apm/new-relic-apm/apdex/apdex-measuring-user-satisfaction
-$app_apdex_threshold = 0.4;
+$app_apdex_threshold = $app_info['settings']['app_apdex_threshold'];
 // Do you want New Relic to add JavaScript to pages to analyze rendering time?
 // https://newrelic.com/browser-monitoring
-$enable_real_user_monitoring = TRUE;
+$enable_real_user_monitoring = $app_info['settings']['enable_real_user_monitoring'];
 // The "t" value (number of seconds) for browser apdex. (The "real user
 // monitoring turned off or on with $enable_real_user_monitoring")
-$end_user_apdex_threshold = 6;
+$end_user_apdex_threshold = $app_info['settings']['end_user_apdex_threshold'];
 
-
-// Do not change these variables.
-$site_name = $_SERVER['PANTHEON_SITE_NAME'];
-$env = $_SERVER['PANTHEON_ENVIRONMENT'];
-$app_name = $site_name . " ($env)";
-
-set_thresholds($app_apdex_threshold, $end_user_apdex_threshold, $enable_real_user_monitoring, $app_name);
-
+set_thresholds( $app_apdex_threshold, $end_user_apdex_threshold, $enable_real_user_monitoring );
 
 /**
  * Gets the New Relic API Key so that further requests can be made.
  */
-function get_api_key() {
-  $return = '';
-  $req = pantheon_curl('https://api.live.getpantheon.com/sites/self/bindings?type=newrelic', NULL, 8443);
-  $meta = json_decode($req['body'], true);
+function get_nr_connection_info() {
+  $output = array();
+  $req    = pantheon_curl( 'https://api.live.getpantheon.com/sites/self/bindings?type=newrelic', null, 8443 );
+  $meta   = json_decode( $req['body'], true );
 
-  foreach($meta as $data) {
-    if ($data['environment'] === PANTHEON_ENVIRONMENT && !empty($data['api_key'])) {
-      $return = $data['api_key'];
-      break;
+  foreach ( $meta as $data ) {
+    if ( $data['environment'] === PANTHEON_ENVIRONMENT ) {
+      if ( empty( $data['api_key'] ) ) {
+        echo "Failed to get API Key\n";
+
+        return;
+      }
+      $output['api_key'] = $data['api_key'];
+
+      if ( empty( $data['app_name'] ) ) {
+        echo "Failed to get app name\n";
+
+        return;
+      }
+      $output['app_name'] = $data['app_name'];
     }
   }
-  return $return;
-};
+
+  return $output;
+}
 
 /**
  * Get the id of the current multidev environment.
  */
-function get_app_id($api_key, $app_name) {
+function get_app_id( $api_key, $app_name ) {
   $return = '';
-  $s = curl_init();
-  curl_setopt($s,CURLOPT_URL,'https://api.newrelic.com/v2/applications.json');
-  curl_setopt($s,CURLOPT_HTTPHEADER,array('X-API-KEY:' . $api_key));
-  curl_setopt($s,CURLOPT_RETURNTRANSFER,1);
-  $result = curl_exec($s);
-  curl_close($s);
+  $s      = curl_init();
+  curl_setopt( $s, CURLOPT_URL, 'https://api.newrelic.com/v2/applications.json' );
+  curl_setopt( $s, CURLOPT_HTTPHEADER, array( 'X-API-KEY:' . $api_key ) );
+  curl_setopt( $s, CURLOPT_RETURNTRANSFER, 1 );
+  $result = curl_exec( $s );
+  curl_close( $s );
 
-  $result = json_decode($result, true);
+  $result = json_decode( $result, true );
 
-  foreach ($result['applications'] as $application) {
-    if ($application['name'] === $app_name ) {
+  foreach ( $result['applications'] as $application ) {
+    if ( $application['name'] === $app_name ) {
       $return = $application['id'];
       break;
     }
   }
+
   return $return;
+}
+
+function get_app_info( $env = 'dev' ) {
+  $nr_connection_info = get_nr_connection_info();
+  if ( empty( $nr_connection_info ) ) {
+    echo "Unable to get New Relic connection info\n";
+
+    return;
+  }
+
+  $api_key  = $nr_connection_info['api_key'];
+  $app_name = $nr_connection_info['app_name'];
+
+  $app_id = get_app_id( $api_key, $app_name );
+
+  $url = "https://api.newrelic.com/v2/applications/$app_id.json";
+
+  $ch = curl_init();
+  curl_setopt( $ch, CURLOPT_URL, $url );
+  curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
+  $headers = [
+      'X-API-KEY:' . $api_key
+  ];
+  curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers );
+
+  $response = curl_exec( $ch );
+
+  if ( curl_errno( $ch ) ) {
+    echo 'Error:' . curl_error( $ch );
+  }
+
+  curl_close( $ch );
+
+  $output = json_decode( $response, true );
+
+  return $output['application'];
 }
 
 /**
  * Sets the apdex thresholds.
  */
-function set_thresholds($app_apdex_threshold, $end_user_apdex_threshold, $enable_real_user_monitoring, $app_name) {
+function set_thresholds( $app_apdex_threshold, $end_user_apdex_threshold, $enable_real_user_monitoring ) {
 
-  $api_key = get_api_key();
-  if (empty($api_key)) {
-    echo "Failed to get API Key";
+  $nr_connection_info = get_nr_connection_info();
+  if ( empty( $nr_connection_info ) ) {
+    echo "Unable to get New Relic connection info\n";
+
     return;
   }
 
-  $app_id = get_app_id($api_key, $app_name);
-  if (empty($app_id)) {
-    echo "Failed to get app id";
-    return;
-  }
+  $api_key  = $nr_connection_info['api_key'];
+  $app_name = $nr_connection_info['app_name'];
+
+  $app_id = get_app_id( $api_key, $app_name );
+
+  echo "===== Setting New Relic Values for the App '$app_name' =====\n";
+  echo "Application Apdex Threshold: $app_apdex_threshold\n";
+  echo "End User Apdex Threshold: $end_user_apdex_threshold\n";
+  echo "Enable Real User Monitoring: $enable_real_user_monitoring\n";
 
   $url = 'https://api.newrelic.com/v2/applications/' . $app_id . '.json';
 
   $settings = [
-    'application' => [
-      'name' => $app_name,
-      'settings' => [
-        'app_apdex_threshold' => $app_apdex_threshold,
-        'end_user_apdex_threshold' => $end_user_apdex_threshold,
-        'enable_real_user_monitoring' => $enable_real_user_monitoring,
+      'application' => [
+          'name'     => $app_name,
+          'settings' => [
+              'app_apdex_threshold'         => $app_apdex_threshold,
+              'end_user_apdex_threshold'    => $end_user_apdex_threshold,
+              'enable_real_user_monitoring' => $enable_real_user_monitoring,
+          ],
       ],
-    ],
   ];
 
-  $data_json = json_encode($settings);
+  $data_json = json_encode( $settings );
 
   $ch = curl_init();
-  curl_setopt($ch, CURLOPT_URL, $url);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-  curl_setopt($ch, CURLOPT_POSTFIELDS, $data_json);
-  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+  curl_setopt( $ch, CURLOPT_URL, $url );
+  curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
+  curl_setopt( $ch, CURLOPT_POSTFIELDS, $data_json );
+  curl_setopt( $ch, CURLOPT_CUSTOMREQUEST, "PUT" );
   $headers = [
-    'X-API-KEY:' . $api_key,
-    'Content-Type: application/json'
+      'X-API-KEY:' . $api_key,
+      'Content-Type: application/json'
   ];
-  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-  $result = curl_exec($ch);
-  if (curl_errno($ch)) {
-    echo 'Error:' . curl_error($ch);
+  curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers );
+  $result = curl_exec( $ch );
+  if ( curl_errno( $ch ) ) {
+    echo 'Error:' . curl_error( $ch );
   }
-  curl_close ($ch);
-}
+  curl_close( $ch );
 
-?>
+  echo "===== Finished Setting New Relic Values =====\n";
+}
