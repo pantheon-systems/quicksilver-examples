c6199a0e85370a3edc7398dcdf4aee2a830dd0b8
Steve Persch <stevector@users.noreply.github.com>
2016-12-14 14:47:18 -0600

    Updating verbiage for D8

--- drush_config_import/drush_config_import.php
+++ drush_config_import/drush_config_import.php
@@ -4,6 +4,6 @@ echo "Importing configuration from yml files...\n";
 passthru('drush config-import -y');
 echo "Import of configuration complete.\n";
 //Clear all cache
-echo "Clearing cache.\n";
+echo "Rebuilding cache.\n";
 passthru('drush cr');
-echo "Clearing cache complete.\n";
+echo "Rebuilding cache complete.\n";
