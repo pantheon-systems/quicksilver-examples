cd29dfdcdb81494a85a5b6690a5541a11f7d5667
Bird-Kid <iblackkite@gmail.com>
2017-07-21 21:17:52 -0400

    Fix undefined variable in new_relic_deploy
    Rename undefined $deploy_tag to $revision from line 53.

--- new_relic_deploy/new_relic_deploy.php
+++ new_relic_deploy/new_relic_deploy.php
@@ -52,7 +52,7 @@ elseif ($_POST['wf_type'] == 'deploy') {
   // Find out if there's a deploy tag:
   $revision = `git describe --tags`;
   // Get the annotation:
-  $changelog = `git tag -l -n99 $deploy_tag`;
+  $changelog = `git tag -l -n99 $revision`;
   $user = $_POST['user_email'];
 }
 
