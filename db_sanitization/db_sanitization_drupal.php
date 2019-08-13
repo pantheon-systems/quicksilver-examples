0b1fb511f28a4fea16625aab0c783befa3aabbc6
mattbloomfield <mbloomfield@uschamber.com>
2019-08-13 12:09:34 -0400

    Fixing a typo in the comment

--- db_sanitization/db_sanitization_drupal.php
+++ db_sanitization/db_sanitization_drupal.php
@@ -1,5 +1,5 @@
 <?php
-// Don't ever santize the database on the live environment. Doing so would
+// Don't ever sanitize the database on the live environment. Doing so would
 // destroy the canonical version of the data.
 if (defined('PANTHEON_ENVIRONMENT') && (PANTHEON_ENVIRONMENT !== 'live')) {
 
