661ed858e58e72c9b0bd703873be6dbf7ac0a2e7
David Needham <david.needham@pantheon.io>
2019-11-25 19:59:08 -0600

    Fixing the Drupal db_sanitization example by removing referenes to config import

--- db_sanitization/db_sanitization_drupal.php
+++ db_sanitization/db_sanitization_drupal.php
@@ -3,7 +3,7 @@
 // destroy the canonical version of the data.
 if (defined('PANTHEON_ENVIRONMENT') && (PANTHEON_ENVIRONMENT !== 'live')) {
 
-	// Import all config changes.
+	// Run the Drush command to sanitize the database.
 	echo "Sanitizing the database...\n";
 	passthru('drush sql-sanitize -y');
 	echo "Database sanitization complete.\n";
