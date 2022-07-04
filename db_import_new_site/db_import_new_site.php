2bb40a57f4a8f0e0873bb4db7b7656bc583d6058
Alex Moreno-Lopez <alejandro.moreno@tdo.es>
2022-07-04 18:28:59 +0200

    Added example of db import when creating a new site from the UI

---
+++ db_import_new_site/db_import_new_site.php
@@ -0,0 +1,10 @@
+<?php
+// Don't ever sanitize the database on the live environment. Doing so would
+// destroy the canonical version of the data.
+if (defined('PANTHEON_ENVIRONMENT') && (PANTHEON_ENVIRONMENT !== 'live')) {
+
+	// Run the Drush command to sanitize the database.
+	echo "Sanitizing the database...\n";
+	passthru('drush sql-sanitize -y');
+	echo "Database sanitization complete.\n";
+}
