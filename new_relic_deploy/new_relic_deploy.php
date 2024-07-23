d0e754bcb534c4e4bf1718b2326afded548988f7
Kevin Porras <kevin.porras@pantheon.io>
2024-07-23 11:04:27 -0600

    Use new deployments API endpoint and query for app_id first. (#195)

--- new_relic_deploy/new_relic_deploy.php
+++ new_relic_deploy/new_relic_deploy.php
@@ -15,6 +15,8 @@ if ($data == false) {
   exit();
 }
 
+$app_id = get_app_id($data['api_key'], $data['app_name']);
+
 // This is one example that handles code pushes, dashboard 
 // commits, and deploys between environments. To make sure we 
 // have good deploy markers, we gather data differently depending
@@ -46,23 +48,31 @@ elseif ($_POST['wf_type'] == 'deploy') {
   $user = $_POST['user_email'];
 }
 
+$deployment_data = [
+  "deployment" => [
+    "revision" => $revision,
+    "changelog" => $changelog,
+    "description" => $description,
+    "user" => $user,
+  ]
+];
 
-// Use New Relic's v1 curl command-line example.
-// TODO: update to use v2 API with JSON, plus curl() in PHP.
-// Blocked by needing the app_id to use v2 API
-$curl = 'curl -H "x-api-key:'. $data['api_key'] .'"';
-$curl .= ' -d "deployment[application_id]=' . $data['app_name'] .'"';
-$curl .= ' -d "deployment[description]= '. $description .'"';
-$curl .= ' -d "deployment[revision]='. $revision .'"';
-$curl .= ' -d "deployment[changelog]='. $changelog .'"';
-$curl .= ' -d "deployment[user]='. $user .'"';
-$curl .= ' https://api.newrelic.com/deployments.xml';
-// The below can be helpful debugging.
-// echo "\n\nCURLing... \n\n$curl\n\n";
+echo "Logging deployment in New Relic App $app_id...\n";
 
-echo "Logging deployment in New Relic...\n";
-passthru($curl);
-echo "Done!";
+$json_data = json_encode($deployment_data, JSON_FORCE_OBJECT);
+echo "Sending: $json_data\n";
+
+$command = "curl -X POST 'https://api.newrelic.com/v2/applications/$app_id/deployments.json' " .
+           "-H 'X-Api-Key: {$data['api_key']}' " .
+           "-H 'Content-Type: application/json' " .
+           "-H 'Accept: application/json' " .
+           "-d '" . $json_data . "'";
+
+echo "Running: $command\n";
+
+passthru($command);
+
+echo "\nDone!\n";
 
 /**
  * Gets the New Relic API Key so that further requests can be made.
@@ -78,4 +88,28 @@ function get_nr_connection_info( $env = 'dev' ) {
   $output['api_key'] = pantheon_get_secret(API_KEY_SECRET_NAME);
 
   return $output;
+}
+
+/**
+ * Get the id of the current multidev environment.
+ */
+function get_app_id( $api_key, $app_name ) {
+  $return = '';
+  $s      = curl_init();
+  curl_setopt( $s, CURLOPT_URL, 'https://api.newrelic.com/v2/applications.json' );
+  curl_setopt( $s, CURLOPT_HTTPHEADER, array( 'X-API-KEY:' . $api_key ) );
+  curl_setopt( $s, CURLOPT_RETURNTRANSFER, 1 );
+  $result = curl_exec( $s );
+  curl_close( $s );
+
+  $result = json_decode( $result, true );
+
+  foreach ( $result['applications'] as $application ) {
+    if ( $application['name'] === $app_name ) {
+      $return = $application['id'];
+      break;
+    }
+  }
+
+  return $return;
 }
