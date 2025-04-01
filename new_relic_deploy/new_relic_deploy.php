ebc53ffacb95ac46150def409789db772c584df0
Brian Weaver <87093053+namespacebrian@users.noreply.github.com>
2025-04-01 13:11:16 -0400

    Strip double quotes from commit message (in addition to single-quotes)

--- new_relic_deploy/new_relic_deploy.php
+++ new_relic_deploy/new_relic_deploy.php
@@ -57,6 +57,7 @@ elseif ($_POST['wf_type'] == 'deploy') {
 $revision = rtrim($revision, "\n");
 $changelog = rtrim($changelog, "\n");
 $changelog = str_replace('\'','',$changelog);
+$changelog = str_replace('"','',$changelog);
 
 $deployment_data = [
   "deployment" => [
